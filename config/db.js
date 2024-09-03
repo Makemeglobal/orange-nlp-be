69227da90c6963023cacc817542527c3e7eb1711
Harsh <harshvchawla997@gmail.com>
2024-09-03 21:23:51 +0530

    pushing

--- config/db.js
+++ config/db.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log('MongoDB connected to' , process.env.MONGO_URI);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
