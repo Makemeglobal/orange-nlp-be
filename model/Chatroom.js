ac55c81e562b39161a1b74e0684c4e1a2372f648
kundan175 <kundansingh957632@gmail.com>
2024-09-17 12:19:31 +0530

    timestamps

--- model/Chatroom.js
+++ model/Chatroom.js
@@ -1,41 +1,46 @@
 const mongoose = require("mongoose");
 
-const chatRoomSchema = new mongoose.Schema({
-  roomId: { type: String, required: true, unique: true },
-  messages: {
-    type: [String],
-    required: true,
-    default: [],
-  },
-  notes: {
-    type: [
-      {
-        text: { type: String, required: false },
-        note: { type: String, required: false },
-        index: {
-          type: Number,
+const chatRoomSchema = new mongoose.Schema(
+  {
+    roomId: { type: String, required: true, unique: true },
+    messages: {
+      type: [String],
+      required: true,
+      default: [],
+    },
+    notes: {
+      type: [
+        {
+          text: { type: String, required: false },
+          note: { type: String, required: false },
+          index: {
+            type: Number,
+          },
+          message: { type: String },
+          timestamp: { type: Date, default: Date.now },
         },
-        message: { type: String },
-        timestamp: { type: Date, default: Date.now },
+      ],
+      default: [],
+    },
+    meetingTitle: { type: String, required: false },
+    summary: { type: String, required: false },
+    meetingPurpose: { type: String, required: false },
+    participants: [
+      {
+        name: { type: String, required: false },
+        title: { type: String, required: false },
       },
     ],
-    default: [],
-  },
-  meetingTitle: { type: String, required: false },
-  summary: { type: String, required: false },
-  meetingPurpose: { type: String, required: false },
-  participants: [
-    {
-      name: { type: String, required: false },
-      title: { type: String, required: false },
+    user: {
+      type: mongoose.Schema.Types.ObjectId, // Data type is ObjectId
+      ref: "User", // Reference to the 'User' model
+      required: false, // This field is not mandatory
     },
-  ],
-  user: {
-    type: mongoose.Schema.Types.ObjectId, // Data type is ObjectId
-    ref: "User", // Reference to the 'User' model
-    required: false, // This field is not mandatory
   },
-});
+  {
+    timestamps: true, // This will add createdAt and updatedAt fields
+  }
+);
 
 const ChatRoom = mongoose.model("ChatRoom", chatRoomSchema);
 
